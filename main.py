7d1dfbd6c1f35c7a6360ba89fcffa2d8d6468b9f
xausssr <xausssr@gmail.com>
2022-11-29 20:22:36 +0300

    fix mistake with f-string

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ async def predict(item: Item, length: int = 120, num_seq: int = 10):
    output = generator(item.text, max_length=length, num_return_sequences=num_seq)
    text = {}
    for text_part in range(len(output)):
-      text["text_part_{text_part}"] = output[text_part].get('generated_text') + '\n'
+      text[f"text_part_{text_part}"] = output[text_part].get('generated_text') + '\n'
    return {"generation_parameters": {"length": length, "num_seq": num_seq}, "generated_text": text}
